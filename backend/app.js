a0f4d2c071cefa506c78d45002416faa671d2d27
jp08 <poncejhon321@gmail.com>
2023-11-02 21:58:00 -0400

    Actualizar dependencias de cambiar ts a js

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+require("dotenv/config");
+const bodyParser = require("body-parser");
+const express = require("express");
+const ClassifierRoutes = require("./src/routes/classifierRoutes");
+const app = express();
+const PORT = 3000 || process.env.PORT;
+const cors = require('cors');
+const allowedOrigins = ['http://localhost:3000'];
+const options = {
+    origin: allowedOrigins,
+};
+app.use(cors(options));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = (server) => {
+    server.use('/api', new ClassifierRoutes().route);
+};
+routes(app);
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
